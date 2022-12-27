49a5704611a43250d0aff1499d1baa8b2c175fc3
muhammedazlan11@gmail.com <muhammedazlan11@gmail.com>
2022-12-27 07:33:04 +0530

    commited

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 alert(' Development in progress');
 const customLanguageDictionaries = {
     english: {
+      "what":"endre",
       "how": "engana",
       "when": "eppa",
       "where": "evde",
-      "how are you": "Rahathalle"
+      "name": "per",
+      "how are you": "Rahathalle",
+      "your": "ninde",
+      "you": "nin",
+      "my": "nande",
+      "me":"nan",
+      
     },
 
   };
@@ -12,22 +19,22 @@ const customLanguageDictionaries = {
     let translatedText = "";
     if (inputLanguage === "custom-language") {
       const dictionaries = customLanguageDictionaries[outputLanguage];
-      // Split the text into words or phrases
+      
       const words = text.split(" ");
       for (const word of words) {
-        // Look up the translation of the word or phrase in the dictionaries
+      
         const translation = dictionaries[word.toLowerCase()];
         if (translation) {
-          // If a translation is found, add it to the translated text
+          
           translatedText += `${translation} `;
         } else {
-          // If no translation is found, leave the word or phrase unchanged
+        
           translatedText += `${word} `;
         }
       }
       translatedText = translatedText.trim();
     } else {
-      // Use a different set of dictionaries or a translation API to translate from a standard language to another language
+      
       translatedText = text;
     }
     return translatedText;
