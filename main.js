46190d4a9cfebde033dae67600065717165d5591
muhammedazlan11@gmail.com <muhammedazlan11@gmail.com>
2022-12-31 13:04:16 +0530

    commited

--- main.js
+++ main.js
@@ -8,15 +8,29 @@ const customLanguageDictionaries = {
       "when": "eppa",
       "where": "evde",
       "name": "per",
-      "how are you": "Rahathalle",
       "your": "ninde",
       "you": "nin",
       "my": "nande",
       "me":"nan",
+      "chell":"say",
+      "yes":"haan",
+      "noo":"alla",
+      "no": "illa",
+      "do":"ak",
+      "new":"pudye",
+      
+      
      //sentences:
         "howareyou": "nin engana ulla",
         "whatisyourname": "ninde per endre",
         "iamnotwell":"nakk husharilla",
+        "whatshouldisay":"nan endre chellon",
+        "doyouknowme":"nikk nande gurtha unda",
+        "mynameis":"nande per",
+        "theydidn'tsayanything": "anga endum chenthla",
+        "hedidn'tsayanything": "awn endum chenthla",
+        "shedidn'tsayanything": "awl endum chenthla",
+        "": "",
     },
 
   };
@@ -28,7 +42,7 @@ const customLanguageDictionaries = {
     const dictionaries = customLanguageDictionaries[outputLanguage];
 
     
-    text = text.replace(/ /g, "");
+   // text = text.replace(/ /g, "");
 
     const words = text.split(" ");
     for (const word of words) {
