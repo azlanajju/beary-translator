d2573d6164532a642b75c25ece191c74ee54bf5b
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-12-31 14:07:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const customLanguageDictionaries = {
     english: {
    
       "what":"endre","how": "engana","when": "eppa","where": "evde","name": "per","your": "ninde","you": "nin","my": "nande","me":"nan","chell":"say","yes":"haan","noo":"alla",
-      "no": "illa","do":"ak","new":"pudye","friend": "changayi","then":"pine","afternoon":"ucche","morning":"subey","night":"jawu","sleep":"keda","get":"kit",
+      "no": "illa","do":"ak","new":"pudye","friend": "changayi","then":"pinne","afternoon":"ucche","morning":"subey","night":"jawu","sleep":"keda","get":"kit",
        "up":"mel","down":"keel","right":"bala","left":"yeda","middle":"nadu","see":"nok","she":"awl","he":"awn","love":"pirsa","like":"ista", "animal":"prani",
        "question":"prashna'","answer":"utthara","box":"petti","go":"po","come":"ba","cold":"sheetha","fever":"pani","cough":"chame","play":"kali","sorry":"maaf","":"",
-      "":"",
-      "":"",
+      "they":"anga",  "which":"ed",  "them":"anga",  "some":"korya",  "home":"aga","apple":"yeppulu","man":"manisa","take":"yed","outside":"parth","inside":"ulga",
+      "want":"benu","sad":"bejar","happy":"kosi","more":"yara","thing":"samana","azlan":"ajju",
       "":"",
       "":"",
       
@@ -17,7 +17,7 @@ const customLanguageDictionaries = {
         "mynameis":"nande per","theydidn'tsayanything": "anga endum chenthla","hedidn'tsayanything": "awn endum chenthla","shedidn'tsayanything": "awl endum chenthla","iloveyou": "nan ninde pirsakra",
         "ilikeyou":"nakk nin ista","willyoubemyfriend":"nin nande changayi awre", "ihaveafewquestions":"nakk korea questions und", 
       "ilikeher":"nakk awl ista","iloveher":"nan awlle pirsakre","ilikehim":"nakk awn ista","ilovehim":"nan awnde pirsa akre","canisaysomething":"na endre ayeng chellata",
-      "":"","":"","":"",
+      "whatcanidoforyou":"nan nikk endre akon","":"","":"",
       "":"","":"","":"","":"","":"","":"","":"",
       "":"","":"","":"","":"","":"","":"","":"",
     },
